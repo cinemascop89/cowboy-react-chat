49d94ddd977daedea5c5b7490187bbea51b7f50b
ed <ed.cinemascop@yahoo.es>
2016-01-24 22:41:14 -0300

    better format timestamps

--- priv/jsx/message.jsx
+++ priv/jsx/message.jsx
@@ -3,7 +3,13 @@ var React = require('react');
 exports.Message = React.createClass({
     formatTimeStamp: function(ts) {
         var date = new Date(ts);
-        return "[" + date.getMinutes() + ":" + date.getSeconds() + "]";
+        var minutes = date.getMinutes();
+        if (minutes < 10)
+            minutes = "0" + minutes;
+        var seconds = date.getSeconds();
+        if (seconds < 10)
+            seconds = "0" + seconds;
+        return `[${minutes}:${seconds}]`;
     },
     render: function() {
         var timestamp = this.formatTimeStamp(this.props.timestamp);
